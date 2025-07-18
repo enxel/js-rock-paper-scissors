d66436a84f730e60cb5a4ae51f514a54f7e5259f
enxel <26006596+enxel@users.noreply.github.com>
2025-07-18 11:26:47 -0600

    Make getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!!");
+// console.log("Hello World!!");
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 1000) % 3;
+    let choice;
+
+    switch(randomNumber) { 
+        case 0:
+            choice = "Rock";
+            break;
+        case 1:
+            choice = "Paper";
+            break;
+        case 2:
+            choice = "Scissors";
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
