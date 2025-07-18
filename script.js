5d01738269e77dc5feab701a893541f14dc7bc80
enxel <26006596+enxel@users.noreply.github.com>
2025-07-18 11:28:12 -0600

    Make playRound function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // console.log("Hello World!!");
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 1000) % 3;
@@ -24,5 +26,24 @@ function getHumanChoice() {
     return choice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.toLowerCase().slice(1);
+
+    if ( (computerChoice === "Rock" && humanChoice === "Scissors") ||
+        (computerChoice === "Paper" && humanChoice === "Rock")    ||
+        (computerChoice === "Scissors" && humanChoice === "Paper")   ) {
+            console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+            computerScore++;
+    } else if ( (computerChoice === "Rock" && humanChoice === "Paper")     ||
+                (computerChoice === "Paper" && humanChoice === "Scissors") ||
+                (computerChoice === "Scissors" && humanChoice === "Rock")     ) {
+                console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+                humanScore++;
+    } else {
+        console.log("It's a tie! Both chose " + computerChoice + ".");
+    }
+}
+
 // console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+playRound(getHumanChoice(), getComputerChoice());
